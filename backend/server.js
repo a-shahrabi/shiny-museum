26707c779138b73439133a394b1dbcd67ff394ef
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-05-18 23:40:50 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
+
 //express app package
 const express = require ('express')
 
 //starting express app
 const app = express()
 
+//route handler (respond to get request)
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+
+})
+
 //listen for request
 app.listen(4000, ()=>{
     console.log('listening on port 4000')
 })
+
