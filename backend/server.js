a4677b0740fcca0e6084a262117dfdfa699f9e68
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-05-26 23:51:27 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const app = express()
 // Middleware for parsing JSON
 app.use(express.json())
 
+// Simple in-memory storage for notes
+let notes = []
+let noteIdCounter = 1
+
 // Middleware
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -20,24 +24,3 @@ app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'})
 })
 
-// About route
-app.get('/about', (req, res) => {
-    res.json({mssg: 'This is the about page'})
-})
-
-// API route with parameter
-app.get('/api/users/:id', (req, res) => {
-    const userId = req.params.id
-    res.json({mssg: `User ID: ${userId}`})
-})
-
-// POST route example
-app.post('/api/data', (req, res) => {
-    const data = req.body
-    res.json({mssg: 'Data received', data: data})
-})
-
-// Listen for request
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
